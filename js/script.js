0c4903b1d83bdea81aa0d12a5ee643637161b9d9
Phil Buchanan <info@philbuchanan.com>
2013-10-10 18:18:40 -0400

    Added swipe gesture for backspace and clear
    - short swipe on result to backspace
    - long swipe on result to clear
    - issue (#34)

--- js/script.js
+++ js/script.js
@@ -15,6 +15,7 @@
 		history: 7,
 		timerlen: 750,
 		timer: null,
+		minswipe: 60,
 		fontsize: 46,
 		url: 'http://ioscalc.com',
 		
@@ -675,6 +676,8 @@
 		buttonModeStart: 'mousedown',
 		buttonModeEnd: 'mouseup',
 		
+		start: {},
+		
 		addEventHandlers: function() {
 		
 			var i;
@@ -711,6 +714,23 @@
 				function() {
 					app.removeTimer();
 					history.close();
+				},
+				false
+			);
+			
+			// Result touch events
+			document.getElementById('result').addEventListener(
+				this.buttonModeStart,
+				function(e) {
+					events.resultTouchStart(e);
+				},
+				false
+			);
+			
+			document.getElementById('result').addEventListener(
+				this.buttonModeEnd,
+				function(e) {
+					events.resultTouchEnd(e);
 				},
 				false
 			);
@@ -758,6 +778,43 @@
 			}
 			else {
 				app.buttonPress(this.value);
+			}
+		
+		},
+		
+		resultTouchStart: function(event) {
+		
+			var d = new Date();
+			
+			this.start = {
+				x: event.changedTouches[0].pageX,
+				y: event.changedTouches[0].pageY,
+				time: d.getTime()
+			};
+		
+		},
+		
+		resultTouchEnd: function(event) {
+		
+			var d = new Date();
+			
+			if (d < this.start.time + 350) {
+			
+				var delta = {
+					x: event.changedTouches[0].pageX - this.start.x,
+					y: event.changedTouches[0].pageY - this.start.y
+				};
+				
+				if (delta.x > settings.minswipe * 4) {
+					app.clear();
+				}
+				else if (delta.x > settings.minswipe &&
+					(Math.abs(delta.y) / Math.abs(delta.x)) < 0.3) {
+						app.backspace();
+				}
+				
+				this.start = {};
+			
 			}
 		
 		}
