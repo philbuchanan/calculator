626ecfe8348bc397bb048311001c2cc5be68f66c
Phil Buchanan <info@philbuchanan.com>
2016-05-03 23:07:22 -0400

    Initial 4.0 commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+/**
+ * Copyright 2013-2016 Phil Buchanan
+ *
+ * A calculator iOS web application that supports brackets, backspace and saved
+ * calculation history. The app uses HTML5 app caching so it will work offline.
+ *
+ * @version 4.0
+ */
