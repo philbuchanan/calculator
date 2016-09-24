74c8aa67ecff9455dea6e07057877649c647d130
Phil Buchanan <info@philbuchanan.com>
2016-09-24 11:42:23 -0400

    #62 Added Vue.js 2.0

---
+++ js/app.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright 2013-2016 Phil Buchanan
+ *
+ * A calculator iOS web application that supports brackets, backspace and saved
+ * calculation history. The app uses HTML5 app caching so it will work offline.
+ *
+ * @version 4.0
+ */
+
+
+
+/**
+ * App Vue
+ *
+ * The main calculator application view. Handles all aspects of the calculator
+ * including keypad, display result and display equation.
+ */
+var app = new Vue({
+	el: '#app'
+})
