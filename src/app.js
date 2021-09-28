50d7732bcdce654a6c0dc521111671d5a324f8c6
Phil Buchanan <info@philbuchanan.com>
2021-09-28 11:51:54 -0400

    Initial v5.0 commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './app.scss';
+
+const App = () => {
+	return (
+		<>
+			<h1>Calculator</h1>
+		</>
+	);
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2013-2021 Phil Buchanan
+ *
+ * A calculator iOS web application that supports brackets, backspace and saved
+ * calculation history. The app uses HTML5 app caching so it will work offline.
+ *
+ * Created using React JS.
+ *
+ * @version 5.0
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app"
+
+ReactDOM.render(<App />, document.getElementById('root'));
